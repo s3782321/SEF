749714c9ff5dded69f8fb2e3179f6f0e0e078aa8
Patrick Bui <s3782321@student.rmit.edu.au>
2020-03-10 17:44:45 +1100

    test project

---
+++ TEST/src/TEST.java
@@ -0,0 +1,29 @@
+
+public class TEST {
+	
+	public static void main(String[] args) {
+	}
+	
+	public void bubbleSort(int[] sortableItems){
+	    int n = sortableItems.length;
+	    for (int i=0; i<n-1; i++) {
+	        for (int j=0; j<n-i-1; j++) {
+	            if (sortableItems[j] > sortableItems[j+1]) {
+	                Swap(sortableItems[j],sortableItems[j+i]);
+	            }
+	        }
+	
+	    }
+	}
+	
+	
+	private void Swap(int a, int b) {
+	    int temp = a;
+	    a = b;
+	    b = temp;
+	    }
+	}
+	
+
+	
+
